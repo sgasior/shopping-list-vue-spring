a443ebd8bed115221823a86ea261bfd5420f7349
Szymon Gąsior <szgasior@outlook.com>
2019-11-07 15:11:02 +0100

    Migrating from jUnit 4 to jUnit 5

---
+++ backend/src/test/pl/edu/kopalniakodu/web/controller/OwnerControllerTest.java
@@ -0,0 +1,16 @@
+package pl.edu.kopalniakodu.web.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class OwnerControllerTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
