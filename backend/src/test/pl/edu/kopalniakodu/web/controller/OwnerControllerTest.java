f2ad8cf468f1c3954f276615ae53ef64728bd4ea
Szymon Gąsior <szgasior@outlook.com>
2019-11-09 17:09:11 +0100

    Creating documentation for the Owner Completed doc for reading all owners and reading owner by id. Created doc for adding owner.

--- backend/src/test/pl/edu/kopalniakodu/web/controller/OwnerControllerTest.java
+++ backend/src/test/pl/edu/kopalniakodu/web/controller/OwnerControllerTest.java
@@ -1,6 +1,7 @@
 package pl.edu.kopalniakodu.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,7 +12,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.constraints.ConstraintDescriptions;
+import org.springframework.restdocs.headers.ResponseHeadersSnippet;
+import org.springframework.restdocs.payload.RequestFieldsSnippet;
 import org.springframework.restdocs.payload.ResponseFieldsSnippet;
+import org.springframework.restdocs.request.PathParametersSnippet;
 import org.springframework.test.web.servlet.MockMvc;
 import pl.edu.kopalniakodu.service.OwnerService;
 import pl.edu.kopalniakodu.web.model.OwnerDto;
@@ -21,19 +26,26 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(RestDocumentationExtension.class)
 @AutoConfigureRestDocs(uriPort = 8088)
 @WebMvcTest(OwnerController.class)
+@Log4j2
 class OwnerControllerTest {
 
     private static final String OWNER_NAME_1 = "John";
@@ -77,14 +89,10 @@ class OwnerControllerTest {
         mockMvc.perform(get("/api/v1/owner/{ownerId}", UUID.randomUUID().toString())
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(ownerDto_1.getName())))
                 .andDo(document("v1/{method-name}",
-                        pathParameters(
-                                parameterWithName("ownerId").description("The unique identifier of the user")
-                        ),
-                        responseFields(
-                                fieldWithPath("id").description("The unique identifier of the user"),
-                                fieldWithPath("name").description("Name of the owner")
-                        )));
+                        ownerPathParametersSnippet(),
+                        ownerResponseFieldsSnippet()));
     }
 
     @Test
@@ -93,12 +101,91 @@ class OwnerControllerTest {
         mockMvc.perform(get("/api/v1/owner/{ownerId}", "-1")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.[0].message", is("Owner with this id: -1 does not exists.")))
+                .andExpect(jsonPath("$.[0].codes", containsInAnyOrder("owner.notfound")))
                 .andDo(document("v1/{method-name}",
-                        pathParameters(
-                                parameterWithName("ownerId").description("The unique identifier of the owner")
-                        ),
+                        ownerPathParametersSnippet(),
                         apiError()));
     }
+
+
+    @Test
+    public void findAllOwners() throws Exception {
+
+        List<OwnerDto> ownerDtoList = Arrays.asList(ownerDto_1, ownerDto_2);
+        Mockito.when(ownerService.findAllOwners()).thenReturn(ownerDtoList);
+
+        mockMvc.perform(get("/api/v1/owner")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].name", is(ownerDto_1.getName())))
+                .andExpect(jsonPath("$.[1].name", is(ownerDto_2.getName())))
+                .andExpect(header().longValue("X-Owners-Total", 2L))
+                .andDo(document("v1/{method-name}", ownerPageHeadersSnippet(), ownerCollectionResponseFieldsSnippet()));
+    }
+
+    @Test
+    public void addOwnerShouldReturnOwnerDtoInBodyResponse() throws Exception {
+
+        Mockito.doNothing().when(ownerService).add(any(OwnerDto.class));
+        String ownerDtoJson = objectMapper.writeValueAsString(ownerDto_1);
+        mockMvc.perform(post("/api/v1/owner")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ownerDtoJson))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").doesNotExist())
+                .andExpect(jsonPath("$.name", is(ownerDto_1.getName())))
+                .andDo(document("v1/{method-name}", ownerRequestFieldsSnippet(), ownerResponseFieldsSnippet()));
+    }
+
+    @Test
+    public void addOwnerShouldReturnErrorWhenNameIsEmpty() throws Exception {
+        Mockito.doNothing().when(ownerService).add(any(OwnerDto.class));
+        String ownerDtoJson = objectMapper.writeValueAsString(OwnerDto.builder().name("").build());
+        mockMvc.perform(post("/api/v1/owner")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ownerDtoJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[*].message", hasItem("must not be blank")))
+                .andDo(document("v1/{method-name}", ownerRequestFieldsSnippet(), apiError()));
+    }
+
+    @Test
+    public void addOwnerShouldReturnErrorWhenNameIsNull() throws Exception {
+        Mockito.doNothing().when(ownerService).add(any(OwnerDto.class));
+        String ownerDtoJson = objectMapper.writeValueAsString(OwnerDto.builder().name(null).build());
+        mockMvc.perform(post("/api/v1/owner")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ownerDtoJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[*].message", hasItem("must not be blank")))
+                .andDo(document("v1/{method-name}", ownerRequestFieldsSnippet(), apiError()));
+    }
+
+    @Test
+    public void addOwnerShouldReturnErrorWhenNameIsTooShort() throws Exception {
+        Mockito.doNothing().when(ownerService).add(any(OwnerDto.class));
+        String ownerDtoJson = objectMapper.writeValueAsString(OwnerDto.builder().name("ab").build());
+        mockMvc.perform(post("/api/v1/owner")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ownerDtoJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[*].message", hasItem("size must be between 3 and 20")))
+                .andDo(document("v1/{method-name}", ownerRequestFieldsSnippet(), apiError()));
+    }
+
+    @Test
+    public void addOwnerShouldReturnErrorWhenNameIsToLong() throws Exception {
+        Mockito.doNothing().when(ownerService).add(any(OwnerDto.class));
+        String ownerDtoJson = objectMapper.writeValueAsString(OwnerDto.builder().name("TO_LOOOOOOOOOOOOOOONG_NAME").build());
+        mockMvc.perform(post("/api/v1/owner")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ownerDtoJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[*].message", hasItem("size must be between 3 and 20")))
+                .andDo(document("v1/{method-name}", ownerRequestFieldsSnippet(), apiError()));
+    }
+
 
     private ResponseFieldsSnippet apiError() {
         return responseFields(
@@ -106,15 +193,35 @@ class OwnerControllerTest {
                 fieldWithPath("[].message").description("A message describing the error").optional());
     }
 
-    @Test
-    public void findAllOwners() throws Exception {
-
-        List<OwnerDto> ownerDtoList = Arrays.asList(ownerDto_1,ownerDto_2);
-        Mockito.when(ownerService.findAllOwners()).thenReturn(ownerDtoList);
-
-        mockMvc.perform(get("/api/v1/owner")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(document("v1/{method-name}"));
+    private RequestFieldsSnippet ownerRequestFieldsSnippet() {
+        ConstraintDescriptions constraintDescriptions = new ConstraintDescriptions(OwnerDto.class);
+        return requestFields(
+                fieldWithPath("name").description("Name of the owner")
+                        .attributes(key("constraints")
+                                .value(constraintDescriptions
+                                        .descriptionsForProperty("name"))));
     }
+
+    private ResponseFieldsSnippet ownerResponseFieldsSnippet() {
+        return responseFields(
+                fieldWithPath("name").description("Name of the owner")
+        );
+    }
+
+    private ResponseFieldsSnippet ownerCollectionResponseFieldsSnippet() {
+        return responseFields(
+                fieldWithPath("[].name").description("Name of the owner")
+        );
+    }
+
+    private PathParametersSnippet ownerPathParametersSnippet() {
+        return pathParameters(
+                parameterWithName("ownerId").description("The unique identifier of the user")
+        );
+    }
+
+    private ResponseHeadersSnippet ownerPageHeadersSnippet() {
+        return responseHeaders(headerWithName("X-Owners-Total").description("The total amount of owners"));
+    }
+
 }
