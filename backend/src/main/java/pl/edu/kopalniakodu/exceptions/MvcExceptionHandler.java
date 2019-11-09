77ef21f452ef7f569673c638b47ad475a0712c24
Szymon Gąsior <szgasior@outlook.com>
2019-11-09 16:01:04 +0100

    Added some exception handlers & fixed ControllerAdvice bug Refactored validationErrorHandler to start using ApiError class. Added extra exception handlers for MethodArgumentNotValidException and Exception. Fixed controller advice by chaning annotation from @ControllerAdvice to @RestControllerAdvice.

--- backend/src/main/java/pl/edu/kopalniakodu/exceptions/MvcExceptionHandler.java
+++ backend/src/main/java/pl/edu/kopalniakodu/exceptions/MvcExceptionHandler.java
@@ -1,23 +1,41 @@
 package pl.edu.kopalniakodu.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolationException;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class MvcExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException ex) {
-        List<String> errorsList = new ArrayList<>(ex.getConstraintViolations().size());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public List<ApiError> validationErrorHandler(ConstraintViolationException ex) {
+        return ex.getConstraintViolations()
+                .stream()
+                .map(err -> new ApiError(err.getPropertyPath().toString(), err.getMessage()))
+                .collect(Collectors.toList());
+    }
 
-        ex.getConstraintViolations().forEach(error -> errorsList.add(error.toString()));
-        return new ResponseEntity<>(errorsList, HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public List<ApiError> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        return ex.getBindingResult()
+                .getAllErrors().stream()
+                .map(err -> new ApiError(err.getCodes(), err.getDefaultMessage()))
+                .collect(Collectors.toList());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public List<ApiError> handleOtherException(Exception ex) {
+        return Collections.singletonList(new ApiError(ex.getClass().getCanonicalName(), ex.getMessage()));
     }
 
 }
