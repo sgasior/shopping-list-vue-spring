e9acc6cd587655850ef35c189a78dbc3d78d01be
Szymon Gąsior <szgasior@outlook.com>
2019-12-12 17:59:25 +0100

    implemented and styled inputs for adding task and products

--- frontend/src/components/AddTask.vue
+++ frontend/src/components/AddTask.vue
@@ -1,14 +1,20 @@
 <template>
   <main>
-    <div class="container add-task">
+    <div class="container task-form">
       <h5 class="center-align header">Add New Task</h5>
       <form class="col s12">
-        <div class="input-field col s12">
-          <input id="task_title" type="text" class="validate" v-model="taskTitle" />
+        <div class="input-field col s12 add-input">
+          <input
+            id="task_title"
+            type="text"
+            v-bind:class="getValidationClassForTask(task)"
+            v-model="task.title"
+          />
           <label for="task_title">Task title</label>
+          <div class="counter">{{task.title.length}}/{{task.maxLen}}</div>
         </div>
         <div class="input-field col s12" :style="cssVars">
-          <select v-model="taskColor">
+          <select v-model="task.color">
             <option value="#fffde1">
               <span class="box box1"></span>
             </option>
@@ -27,26 +33,37 @@
           </select>
         </div>
         <label>Choose the color of your task</label>
-        <div v-for="(product,index) in products" :key="index" class="field col s12">
-          <input id="product" type="text" v-model="products[index]" />
-          <label for="product">Product</label>
+        <div v-for="(product,index) in products" :key="index" class="input-field col s12">
+          <input
+            id="product"
+            type="text"
+            v-bind:class="getValidationClassForProduct(products[index])"
+            v-model="products[index].name"
+          />
+          <div class="product-name">Product</div>
+          <div class="counter">{{products[index].name.length}}/{{task.maxLen}}</div>
           <i class="material-icons delete" @click="deleteProduct(index)">delete</i>
         </div>
         <div class="input-field col s12">
           <input
             id="add_product"
             type="text"
-            class="validate"
+            v-bind:class="getValidationClassForProduct(product)"
             @keydown.tab.prevent="addProduct"
-            v-model="productName"
+            v-model="product.name"
           />
-          <label for="add_produc">Add an product (press TAB or proper icon):</label>
+          <div class="counter">{{product.name.length}}/{{product.maxLen}}</div>
+          <label for="add_product">Add an product (press TAB or proper icon):</label>
+          <i class="material-icons add" @click="addProduct()">add_circle</i>
+          <div :style="{visibility: error ? 'visible':'hidden'}">
+            <div class="error">{{error}}</div>
+          </div>
         </div>
         <a
           class="waves-effect waves-light btn-large btn-publish btn-add-task"
-          v-bind:class="allFieldsAreValid ? '' :'disabled'"
+          v-bind:class="inputsAreValid ? '' :'disabled'"
           @click="publishTask()"
-        >Publish Task</a>
+        >Save Task</a>
       </form>
     </div>
   </main>
@@ -56,26 +73,67 @@
 export default {
   data() {
     return {
-      taskTitle: "",
-      taskColor: "#fffde1",
       products: [],
-      productName: null,
-      allFieldsAreValid: false
+      product: {
+        name: "",
+        minLen: 3,
+        maxLen: 50
+      },
+      allFieldsAreValid: false,
+      task: {
+        title: "",
+        color: "#fffde1",
+        minLen: 3,
+        maxLen: 50
+      },
+      error: null
     };
   },
   methods: {
     addProduct() {
-      if (this.productName == null) {
-        console.log("null jest");
+      if (this.product.name == "") {
+        this.error = "You must enter a product name before add";
       } else {
-        this.products.push(this.productName);
-        this.productName = null;
+        let prod = {};
+        prod.name = this.product.name;
+        this.products.push(prod);
+        this.product.name = "";
+        this.error = null;
       }
     },
     deleteProduct(index) {
       this.products = this.products.filter(product => {
         return product !== this.products[index];
       });
+    },
+    getValidationClassForTask(taskToCheck) {
+      if (taskToCheck.title.length == 0) {
+        return "";
+      }
+      if (
+        taskToCheck.title.length < this.task.minLen ||
+        taskToCheck.title.length > this.task.maxLen
+      ) {
+        return "invalid";
+      } else {
+        return "valid";
+      }
+    },
+    getValidationClassForProduct(prodToCheck) {
+      if (prodToCheck.name.length == 0) {
+        if (this.products.includes(prodToCheck)) {
+          return "invalid";
+        }
+        return "";
+      }
+      if (
+        prodToCheck.name.length < this.product.minLen ||
+        prodToCheck.name.length > this.product.maxLen
+      ) {
+        return "invalid";
+      } else {
+        return "valid";
+      }
     }
   },
   mounted() {
@@ -84,8 +142,28 @@ export default {
   computed: {
     cssVars() {
       return {
-        "--task-bg-color": this.taskColor
+        "--task-bg-color": this.task.color
       };
+    },
+    inputsAreValid() {
+      if (
+        this.task.title.length < this.task.minLen ||
+        this.task.title.length > this.task.maxLen ||
+        this.task.title == null ||
+        this.products.length == 0
+      ) {
+        return false;
+      }
+      for (let i = 0; i < this.products.length; i++) {
+        if (
+          this.products[i].name.length < this.product.minLen ||
+          this.products[i].name.length > this.product.maxLen ||
+          this.products[i].name == null
+        ) {
+          return false;
+        }
+      }
+      return true;
     }
   }
 };
@@ -101,10 +179,10 @@ export default {
   fill: #827717;
 }
 
-.add-task {
+.task-form {
   max-width: 600px;
 }
-.add-task .header {
+.task-form .header {
   font-size: 1.8rem;
   margin-top: 8rem;
   margin-bottom: 4rem;
@@ -113,27 +191,91 @@ export default {
   -webkit-text-fill-color: transparent;
 }
 
-.add-task .input-field {
+.task-form .input-field {
   margin-top: 3rem;
 }
 
 .defaultOption {
   color: #9e9e9e;
 }
+/* label underline focus color */
+.task-form .input-field input[type="text"]:focus {
+  border-bottom: 1px solid #9e9e9e;
+  box-shadow: 0 1px 0 0 #9e9e9e;
+}
 
-.add-task .field {
+/* label focus color */
+.task-form .input-field input[type="text"]:focus + label {
+  color: #9e9e9e;
+}
+
+/* valid color */
+.task-form .input-field input[type="text"].valid {
+  border-bottom: 1px solid #9ccc65;
+  box-shadow: 0 1px 0 0 #9ccc65;
+}
+
+/* invalid color */
+.task-form .input-field input[type="text"].invalid {
+  border-bottom: 1px solid orangered;
+  box-shadow: 0 1px 0 0 orangered;
+}
+
+.task-form .field {
   position: relative;
 }
-.add-task .delete {
+.task-form .delete {
   position: absolute;
   right: 0;
   bottom: 35px;
+}
+
+.task-form .add {
+  position: absolute;
+  right: 0;
+  bottom: 35px;
+  cursor: pointer;
+  color: #e6a400;
+  transition: all 0.3s;
+}
+
+.task-form .add:hover {
+  color: #e6b31e;
+  transform: scale(1.1);
+}
+
+.counter,
+.product-name,
+.error {
+  font-size: 0.8rem;
+  text-align: right;
+  display: inline-block;
+}
+
+.error {
+  color: orangered;
+}
+
+.product-name {
+  color: #9e9e9e;
+}
+
+.counter {
+  color: #9e9e9e;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+}
+
+.add-input .counter {
+  bottom: -23px;
 }
 
 .btn-add-task {
   margin: 3rem 0;
   width: 100%;
 }
+
 .box {
   width: 20px;
   height: 20px;
